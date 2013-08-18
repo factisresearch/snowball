5e9691f4fe19fa10bc8923c03d008ef9f54b7b26
Dag Odenhall <dag.odenhall@gmail.com>
2013-08-18 11:45:25 +0000

    port fixture tests to async

--- test/snowball-fixtures.hs
+++ test/snowball-fixtures.hs
@@ -1,18 +1,18 @@
 {-# LANGUAGE StandaloneDeriving #-}
 
 -------------------------------------------------------------------------------
-import           Control.Applicative           ((<$>))
-import           Control.Concurrent.ParallelIO (parallel, stopGlobalPool)
-import           Control.Monad                 (forM)
+import           Control.Applicative      ((<$>))
+import           Control.Concurrent.Async (mapConcurrently)
+import           Control.Monad            (forM)
 -------------------------------------------------------------------------------
-import           Data.Char                     (toUpper)
-import qualified Data.Text                     as Text
-import qualified Data.Text.IO                  as Text
+import           Data.Char                (toUpper)
+import qualified Data.Text                as Text
+import qualified Data.Text.IO             as Text
 -------------------------------------------------------------------------------
-import           NLP.Snowball                  (Algorithm (..), stem)
+import           NLP.Snowball             (Algorithm (..), stem)
 -------------------------------------------------------------------------------
-import           System.Environment            (getArgs)
-import           System.FilePath               (dropExtension, takeFileName)
+import           System.Environment       (getArgs)
+import           System.FilePath          (dropExtension, takeFileName)
 -------------------------------------------------------------------------------
 
 
@@ -30,14 +30,13 @@ pairs _ = []
 main :: IO ()
 main = do
     args <- getArgs
-    tests <- parallel $ flip map args $ \file -> do
+    tests <- flip mapConcurrently args $ \file -> do
       let name = dropExtension $ takeFileName file
           algorithm = read $ (toUpper $ head name) : (tail name)
       ws <- Text.words <$> Text.readFile file
       forM (pairs ws) $ \(word,expected) -> do
         let stemmed = stem algorithm word
         return $ stemmed == expected
-    stopGlobalPool
     let hits = length $ filter id $ concat tests
         misses = length $ filter not $ concat tests
     putStrLn $ "Hits: " ++ show hits
--- test/stemmer-fixtures.hs
+++ test/stemmer-fixtures.hs
@@ -1,16 +1,16 @@
 {-# LANGUAGE StandaloneDeriving #-}
 
 -------------------------------------------------------------------------------
-import           Control.Applicative           ((<$>))
-import           Control.Concurrent.ParallelIO (parallel, stopGlobalPool)
-import           Control.Monad                 (forM)
+import           Control.Applicative      ((<$>))
+import           Control.Concurrent.Async (mapConcurrently)
+import           Control.Monad            (forM)
 -------------------------------------------------------------------------------
-import           Data.Char                     (toUpper)
+import           Data.Char                (toUpper)
 -------------------------------------------------------------------------------
-import           NLP.Stemmer                   (Stemmer (..), stem)
+import           NLP.Stemmer              (Stemmer (..), stem)
 -------------------------------------------------------------------------------
-import           System.Environment            (getArgs)
-import           System.FilePath               (dropExtension, takeFileName)
+import           System.Environment       (getArgs)
+import           System.FilePath          (dropExtension, takeFileName)
 -------------------------------------------------------------------------------
 
 
@@ -28,14 +28,13 @@ pairs _ = []
 main :: IO ()
 main = do
     args <- getArgs
-    tests <- parallel $ flip map args $ \file -> do
+    tests <- flip mapConcurrently args $ \file -> do
       let name = dropExtension $ takeFileName file
           algorithm = read $ (toUpper $ head name) : (tail name)
       ws <- words <$> readFile file
       forM (pairs ws) $ \(word,expected) -> do
         let stemmed = stem algorithm word
         return $ stemmed == expected
-    stopGlobalPool
     let hits = length $ filter id $ concat tests
         misses = length $ filter not $ concat tests
     putStrLn $ "Hits: " ++ show hits
