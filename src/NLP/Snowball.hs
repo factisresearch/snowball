60c2b1e7186335ecd7fc1d4021ac3dfbd4953ca6
Dag Odenhall <dag.odenhall@gmail.com>
2013-08-25 14:00:33 +0000

    coding style

--- src/NLP/Snowball.hs
+++ src/NLP/Snowball.hs
@@ -1,7 +1,7 @@
 -- |
---   Portability: Haskell2010
+-- Portability: Haskell2010
 --
---   Bindings to the Snowball library.
+-- Bindings to the Snowball library.
 module NLP.Snowball
     ( -- * Pure interface
       Algorithm(..)
@@ -12,27 +12,20 @@ module NLP.Snowball
     , newStemmer
     , stemWith
     , stemsWith
-    )
-  where
+    ) where
 
--------------------------------------------------------------------------------
 import           Control.Concurrent    (MVar, newMVar, withMVar)
 import           Control.Monad         (when)
--------------------------------------------------------------------------------
 import qualified Data.ByteString       as ByteString
 import           Data.ByteString.Char8 (ByteString, pack, packCStringLen,
                                         useAsCString)
 import           Data.Text             (Text)
 import           Data.Text.Encoding    (decodeUtf8, encodeUtf8)
 import           Data.Traversable      (Traversable, forM)
--------------------------------------------------------------------------------
 import           Foreign               (ForeignPtr, FunPtr, Ptr, newForeignPtr,
                                         nullPtr, withForeignPtr)
 import           Foreign.C             (CInt (..), CString)
--------------------------------------------------------------------------------
 import           System.IO.Unsafe      (unsafePerformIO)
--------------------------------------------------------------------------------
-
 
 -- | Snowball algorithm used for stemming words.
 data Algorithm
@@ -55,68 +48,60 @@ data Algorithm
 
 -- | Compute the stem of a word using the specified algorithm.
 --
---   >>> stem English "fantastically"
---   "fantast"
+-- >>> stem English "fantastically"
+-- "fantast"
 stem :: Algorithm -> Text -> Text
 stem algorithm word = let [a] = stems algorithm [word] in a
 
 -- | Compute the stems of several words in one go.  This can be more
---   efficient than @'map' 'stem'@ because it uses a single 'Stemmer'
---   instance, however the @map@ version is rewritten to use this function
---   with a rewrite rule.  You can still use this function though if you
---   want to make sure it is used or if you find it more convenient.
+-- efficient than @'map' 'stem'@ because it uses a single 'Stemmer'
+-- instance, however the @map@ version is rewritten to use this function
+-- with a rewrite rule.  You can still use this function though if you
+-- want to make sure it is used or if you find it more convenient.
 stems :: (Traversable t) => Algorithm -> t Text -> t Text
-stems algorithm ws =
-    unsafePerformIO $
-      do stemmer <- newStemmer algorithm
-         stemsWith stemmer ws
+stems algorithm ws = unsafePerformIO $ do
+    stemmer <- newStemmer algorithm
+    stemsWith stemmer ws
 
 {-# RULES "map/stem" forall a. map (stem a) = stems a #-}
-
-
--------------------------------------------------------------------------------
 
 -- | A thread and memory safe Snowball stemmer instance.
 newtype Stemmer = Stemmer (MVar (ForeignPtr SbStemmer))
 
 -- | Create a new reusable 'Stemmer' instance.
-newStemmer :: Algorithm  -> IO Stemmer
+newStemmer :: Algorithm -> IO Stemmer
 newStemmer algorithm =
     useAsCString (algorithmName algorithm) $ \name ->
-      useAsCString (pack "UTF_8") $ \utf8 ->
-        do sb_stemmer <- sb_stemmer_new name utf8
-           when (sb_stemmer == nullPtr) $
-             error "NLP.Snowball.newStemmer: nullPtr"
-           structPtr <- newForeignPtr sb_stemmer_delete sb_stemmer
-           mvar <- newMVar structPtr
-           return $ Stemmer mvar
+    useAsCString (pack "UTF_8") $ \utf8 -> do
+        sb_stemmer <- sb_stemmer_new name utf8
+        when (sb_stemmer == nullPtr) $ error "NLP.Snowball.newStemmer: nullPtr"
+        foreignPtr <- newForeignPtr sb_stemmer_delete sb_stemmer
+        mvar <- newMVar foreignPtr
+        return $ Stemmer mvar
 
 -- | Use a 'Stemmer' to stem a word.  This can be used more efficiently
---   than 'stem' because you can keep a stemmer around and reuse it, but it
---   requires 'IO' to ensure thread safety.
+-- than 'stem' because you can keep a stemmer around and reuse it, but it
+-- requires 'IO' to ensure thread safety.
 stemWith :: Stemmer -> Text -> IO Text
 stemWith stemmer word = do
     [a] <- stemsWith stemmer [word]
     return a
 
 -- | Use a 'Stemmer' to stem multiple words in one go.  This can be more
---   efficient than @'mapM' 'stemWith'@ because the 'Stemmer' is only
---   locked once.
+-- efficient than @'mapM' 'stemWith'@ because the 'Stemmer' is only
+-- locked once.
 stemsWith :: (Traversable t) => Stemmer -> t Text -> IO (t Text)
 stemsWith (Stemmer mvar) ws =
-    withMVar mvar $ \structPtr ->
-      withForeignPtr structPtr $ \sb_stemmer ->
-        forM ws $ \word -> do
-          let word' = encodeUtf8 word
-          useAsCString word' $ \word'' ->
-            do ptr <- sb_stemmer_stem sb_stemmer word'' $
-                        fromIntegral $ ByteString.length word'
-               len <- sb_stemmer_length sb_stemmer
-               bytes <- packCStringLen (ptr,fromIntegral len)
-               return $ decodeUtf8 bytes
-
-
--------------------------------------------------------------------------------
+    withMVar mvar $ \foreignPtr ->
+    withForeignPtr foreignPtr $ \sb_stemmer ->
+    forM ws $ \word -> do
+        let word' = encodeUtf8 word
+        useAsCString word' $ \word'' -> do
+            let size = fromIntegral $ ByteString.length word'
+            ptr <- sb_stemmer_stem sb_stemmer word'' size
+            len <- sb_stemmer_length sb_stemmer
+            bytes <- packCStringLen (ptr,fromIntegral len)
+            return $ decodeUtf8 bytes
 
 data SbStemmer
 
@@ -133,21 +118,19 @@ foreign import ccall unsafe "sb_stemmer_length"
     sb_stemmer_length :: Ptr SbStemmer -> IO CInt
 
 algorithmName :: Algorithm -> ByteString
-algorithmName algorithm =
-    case algorithm of
-      Danish     -> pack "da"
-      Dutch      -> pack "nl"
-      English    -> pack "en"
-      Finnish    -> pack "fi"
-      French     -> pack "fr"
-      German     -> pack "de"
-      Hungarian  -> pack "hu"
-      Italian    -> pack "it"
-      Norwegian  -> pack "no"
-      Portuguese -> pack "pt"
-      Romanian   -> pack "ro"
-      Russian    -> pack "ru"
-      Spanish    -> pack "es"
-      Swedish    -> pack "sv"
-      Turkish    -> pack "tr"
-      Porter     -> pack "porter"
+algorithmName Danish     = pack "da"
+algorithmName Dutch      = pack "nl"
+algorithmName English    = pack "en"
+algorithmName Finnish    = pack "fi"
+algorithmName French     = pack "fr"
+algorithmName German     = pack "de"
+algorithmName Hungarian  = pack "hu"
+algorithmName Italian    = pack "it"
+algorithmName Norwegian  = pack "no"
+algorithmName Portuguese = pack "pt"
+algorithmName Romanian   = pack "ro"
+algorithmName Russian    = pack "ru"
+algorithmName Spanish    = pack "es"
+algorithmName Swedish    = pack "sv"
+algorithmName Turkish    = pack "tr"
+algorithmName Porter     = pack "porter"
